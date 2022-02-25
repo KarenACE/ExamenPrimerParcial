76297a6e54f368801333ab1fc9b1c0575d9f7385
KarenACE <80796792+KarenACE@users.noreply.github.com>
2022-02-24 22:37:12 -0600

    Tuneles y Score

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     float speed = 3.0f;
-     [SerializeField]
-     float jumpForce = 7.0f;
-      Rigidbody2D rb2D;
-      SpriteRenderer spr;
-       Animator anim;
+    [SerializeField]
+    float jumpForce = 7.0f;
+    Rigidbody2D rb2D;
+    SpriteRenderer spr;
+    Animator anim;
+
+    [SerializeField,Range (0.01f,10f)]
+    float rayDistance = 2f;
+    [SerializeField]
+    Color rayColor = Color.red;
+    [SerializeField]
+    LayerMask groundLayer;
+    [SerializeField]
+    Vector3 rayOrigin;
+    [SerializeField]
+    Score score; 
+
+
+    
 
       void Start()
       {
@@ -22,18 +36,46 @@ public class Player : MonoBehaviour
 
            void Update()
                { 
-               transform.Translate(Vector2.right*AxisRaw.x * speed * Time.deltaTime);
-        
+               transform.Translate(Vector2.right * AxisRaw.x * speed * Time.deltaTime);
                if(JumpButton)
                { 
                    rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); 
+                   anim.SetTrigger("jump"); 
+                  
                }
                spr.flipX = FlipSprite;
-               anim.SetFloat("AxisX", Mathf.Abs(Axis.x));
+               anim.SetFloat("AxisX",Mathf.Abs(Axis.x));
+              
+               
                }
+               void LateUpdate()
+                   {
+anim.SetFloat("AxisX",Mathf.Abs(Axis.x));
+anim.SetBool("ground",IsGrounding); 
+
+                   }
+               
 
 Vector2 Axis => new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
 Vector2 AxisRaw => new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
 bool JumpButton => Input.GetButtonDown("Jump");
 bool FlipSprite => Axis.x > 0 ? false : Axis.x < 0 ? true: spr.flipX;
+bool IsGrounding => Physics2D.Raycast(transform.position + rayOrigin,Vector3.down,rayDistance,groundLayer);
+
+void OnTriggerEnter2D(Collider2D col)
+ {
+    if(col.CompareTag("egg"))
+    {
+       egg egg = col.GetComponent<egg>();
+       score.AddPoints(egg.GetPoints);
+       Destroy(col.gameObject);
+   }
+
+ } 
+
+ void OnDrawGizmosSelected()
+ {
+     Gizmos.color = rayColor;
+     Gizmos.DrawRay(transform.position + rayOrigin,Vector3.down * rayDistance);
+ }
 }
---
+++ Assets/Scripts/egg.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class egg : MonoBehaviour
+{
+   
+    [SerializeField]
+    int points =3;
+
+    public int  GetPoints => points;
+
+}
